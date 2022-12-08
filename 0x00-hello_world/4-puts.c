c5234c5c987d863a130e0c9dd176a9c3669fec3b
babajideawoseemo <akinloluolaposi@gmail.com>
2022-12-08 00:32:35 -0800

    Print into a file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * puts - instead of using printf - is used to output the value "Programming is like building a multilingual puzzle"
+ * and returns a value 0
+ */
+
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return  (0);
+}
