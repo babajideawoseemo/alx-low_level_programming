91cce88ab598e60a6cf79afeea68fa97db8c121c
babajideawoseemo <akinloluolaposi@gmail.com>
2022-12-08 01:54:08 -0800

    Print files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
-*main: Entry point
-*Output: (0)
+*main - Main function
+*
+*
+*Return: return 0 everytime
 */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return (0);
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
 }
