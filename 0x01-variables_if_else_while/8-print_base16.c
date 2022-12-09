3094e72e87b7e899bbc1cb18d8e78cf595dd78f8
babajideawoseemo <akinloluolaposi@gmail.com>
2022-12-09 06:41:50 -0800

    print values in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*main - Entry Point
+*
+*Return: Always 0.
+*/
+int main(void)
+{
+char b16;
+for (b16 = 48; b16 <= 57; b16++)
+{
+putchar(b16);
+}
+for (b16 = 97; b16 <= 102; b16++)
+{
+putchar(b16)
+}
+putchar('\n');
+return (0);
+}
