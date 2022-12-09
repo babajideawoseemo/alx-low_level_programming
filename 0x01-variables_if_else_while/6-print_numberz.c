bf9a650ad1b14a7006b6c2470a4bd3ca341d5184
babajideawoseemo <akinloluolaposi@gmail.com>
2022-12-09 05:53:11 -0800

    added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - Entry Point
+*
+*Return: Alwsys 0;
+*/
+int main(void)
+{
+int num;
+for( num = 48; num <= 59; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
