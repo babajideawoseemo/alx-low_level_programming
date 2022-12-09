e772d244997657dc0b3a8fae6e31a13910866c5f
babajideawoseemo <akinloluolaposi@gmail.com>
2022-12-09 06:13:15 -0800

    Print alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main - Entry Point
+*
+*Return: Always 0.
+*/
+int main(void)
+{
+char value;
+
+for (value = 120; value>= 95; value--)
+{
+putchar(value);
+}
+putchar('\n');
+return (0);
+}
