45c2d880816c2c6d4ac06e9da48e1b075b827f89
babajideawoseemo <akinloluolaposi@gmail.com>
2022-12-09 04:15:18 -0800

    Print alphabet without q n e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,12 +9,13 @@
 int main(void)
 {
 char value;
-for(value = 'a';value <= 'z';value++)
+for(value = 'a'; value <= 'z'; value++)
 {
 if(value != 'e' && value != 'q')
 {
 putchar(value);
-}putchar('\n');
-return (0);
 }
+}
+putchar('\n');
+return (0);
 }
