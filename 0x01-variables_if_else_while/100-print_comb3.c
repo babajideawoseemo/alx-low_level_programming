ab14f3a7e54b6fe76d074392e8db5fcb4c8e20a9
babajideawoseemo <akinloluolaposi@gmail.com>
2022-12-09 10:23:11 -0800

    Print digit combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,14 +6,13 @@
 * Description: prints two digits combination for digits
 * Return: Always 0 (success)
 */
-
-int main(void) 
+int main(void)
 {
 int c, i;
 for (c = '0'; c <= '9'; c++)
 {
 for (i = '0'; i <= '9'; i++)
-{	  
+{
 if (c < i)
 {
 putchar(c);
