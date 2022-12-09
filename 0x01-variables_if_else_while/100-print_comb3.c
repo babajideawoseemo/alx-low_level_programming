a3793f23b5539685d79337e28dc6fe768905abcc
babajideawoseemo <akinloluolaposi@gmail.com>
2022-12-09 10:21:46 -0800

    Print digit combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,22 +2,23 @@
 #include <unistd.h>
 
 /**
-*main - Entry point
-*Description: prints two digits combination
-*Return: Always 0
+* main - Entry point for our code
+* Description: prints two digits combination for digits
+* Return: Always 0 (success)
 */
-int main(void)
+
+int main(void) 
 {
-int first_value,second_value;
-for (first_value = '0'; first_value  <= '9'; first_value++)
+int c, i;
+for (c = '0'; c <= '9'; c++)
 {
-for (second_value = '0'; second_value <= '9'; second_value++)
+for (i = '0'; i <= '9'; i++)
+{	  
+if (c < i)
 {
-if (first_value < second_value)
-{
-putchar(first_value);
-putchar(second_value);
-if (first_value != '8' || (first_value == '8' && second_value != '9'))
+putchar(c);
+putchar(i);
+if (c != '8' || (c == '8' && i != '9'))
 {
 putchar(',');
 putchar(' ');
