74861fe5c78cb31213f8229fdeb3684c19354823
babajideawoseemo <akinloluolaposi@gmail.com>
2022-12-09 03:07:35 -0800

    Generate random number and print last digit stored

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main -> assign a random number to the variable after execution
+* and print the last digit of the number stored in variable n
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int number;
+for(number = 0; number < 10; number++){
+printf("%d", number);
+}
+printf("\n");
+return (0);
+}
