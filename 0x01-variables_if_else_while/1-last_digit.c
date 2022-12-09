6f21ea98beae86dd4bd1d49725141235821567e7
babajideawoseemo <akinloluolaposi@gmail.com>
2022-12-09 03:36:26 -0800

    ast digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,28 +6,26 @@
 * and print the last digit of the nunber stored in the variable n
 * Return: Always 0 (Success)
 */
-int main(void)
-  
+int main(void) 
 {
+int n;
+int lastDigit;
   
- int n;
- int lastDigit;
-  
- srand(time(0));
-  
- n = rand() - RAND_MAX / 2;
- lastDigit = n % 10;
- printf("Last digit of %d is %d ", n, lastDigit);
- if(x > 5){
- printf("and is greater than 5");
- }
- if(x == 0) {
- printf("and is 0");
- }
- if(x < 6 && x != 0) {
- printf("and is less than 6 and not 0");
-  }
-  printf("\n");
-  return (0);
+srand(time(0));
+ 
+n = rand() - RAND_MAX / 2;
+lastDigit = n % 10;
+printf("Last digit of %d is %d ", n, lastDigit);
+if(lastDigit > 5){
+printf("and is greater than 5");
+}
+if(lastDigit == 0) {
+printf("and is 0");
+}
+if(lastDigit < 6 && lastDigit != 0) {
+printf("and is less than 6 and not 0");
+}
+printf("\n");
+return (0);
   
 }
