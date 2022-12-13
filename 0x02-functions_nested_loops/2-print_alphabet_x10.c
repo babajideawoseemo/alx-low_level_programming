192848783e2609609a2bba3c7469420dcaba47c6
babajideawoseemo <akinloluolaposi@gmail.com>
2022-12-13 04:11:31 -0800

    print alphabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,23 @@
-#include "main.h"
-
-/**
- *print_alphabet_x10 - Print alphabets 10 times
- *
- *Return: void
- */
-void print_alphabet_x10(void)
-{
-int i = 0;
-char c;
-for (i = 0; i < 10; i++){
-for(c = 'a'; c = 'z';c++){
-_putchar(c);
-}
-_putchar('\n');
-}
+#include "main.h" 
+  
+/** 
+  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase, 
+  * followed by a new line 
+  */ 
+void print_alphabet_x10(void) 
+{ 
+char ch; 
+int i; 
+i = 0; 
+while (i < 10) 
+{ 
+ch = 'a'; 
+while (ch <= 'z') 
+{ 
+_putchar(ch); 
+ch++; 
+} 
+_putchar('\n'); 
+i++; 
+} 
 }
