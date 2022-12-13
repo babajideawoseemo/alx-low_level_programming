b8cb863fecefb6c4bbfa7a604cf91cd9d922196d
babajideawoseemo <akinloluolaposi@gmail.com>
2022-12-13 12:55:23 -0800

    print multiples of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - prints the sum of all multiples of 3 or 5 up to 1024
+ *
+ *Return: Always.
+ */
+int main(void)
+{
+int i , count = 0;
+for (i = 0; i < 1024; i++)
+{
+if((i % 3 == 0) || (i % 5 == 0))
+{
+count += i;
+}
+}
+printf("%d\n");
+return (0);
+}
