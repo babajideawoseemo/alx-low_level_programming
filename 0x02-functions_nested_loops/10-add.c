3a24d65e267fbbddd7e34f8dfd3b58a19d6a4015
babajideawoseemo <akinloluolaposi@gmail.com>
2022-12-13 08:21:29 -0800

    add 2 nos

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *add - add 2 integer
+ *@a: first parameter
+ *@b: second parameter
+ *Description: add 2 numbers
+ *Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
