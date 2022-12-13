5998a623612ba056eaef51fde576422f55245077
babajideawoseemo <akinloluolaposi@gmail.com>
2022-12-13 04:45:04 -0800

    print alphabet 10x

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- *_islower - Entry point
- *Description: checks if lower
- *by the value it receives
- *Return: 1 if true and 0 if false.
- */
+*_islower - Entry point
+*@c: teh character to check
+*Description: checks if lower
+*by the value it receives
+*Return: 1 if true and 0 if false.
+*/
 int _islower(int c)
 {
 int value = 'a';
 for (value = 'a'; value <= 'z'; value++){
 if (c == value){
 return (1);
-}
-else {
-return (0);
 }
 }
 return (0);
