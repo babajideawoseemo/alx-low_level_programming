4269c437bf03c38f26215513d95bf61dce7f5acb
babajideawoseemo <akinloluolaposi@gmail.com>
2022-12-13 08:11:14 -0800

    print times table for 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@
 void times_table(void)
 {
 int r,c,d;
-for(r=0; r < 10; r++)
+for(r = 0; r < 10; r++)
 {
 _putchar('0');
 _putchar(',');
@@ -17,7 +17,7 @@ for (c = 1; c < 10; c++)
 d = r * c;
 if ((d / 10) > 0)
 {
-_putchar((d/10) + 48);
+_putchar((d / 10) + 48);
 }
 else
 {
