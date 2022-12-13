ebb4a3123fd2dedb72d31a83a082903c19c47a1e
babajideawoseemo <akinloluolaposi@gmail.com>
2022-12-13 13:02:17 -0800

    print multiples of 3 or 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-int i , count = 0;
+int i, count = 0;
 for (i = 0; i < 1024; i++)
 {
-if((i % 3 == 0) || (i % 5 == 0))
+if ((i % 3 == 0) || (i % 5 == 0))
 {
 count += i;
 }
 }
- printf("%d\n", count);
+printf("%d\n", count);
 return (0);
 }
