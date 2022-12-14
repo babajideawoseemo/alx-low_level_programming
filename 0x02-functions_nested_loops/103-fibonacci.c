88e7667fb5f17ca65daa3f0368617757e095f55a
babajideawoseemo <akinloluolaposi@gmail.com>
2022-12-13 18:44:55 -0800

    Fibonacci of even numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - prints sum of even fibonacci
+ * fibonacci suit numbers
+ *Return: return 0
+*/
+
+int main(void)
+{
+unsigned long num1 = 0, num2 = 1, num3 = 0, total = 0;
+while (num3 <= 400000)
+{
+num3 = num2 + num1;
+num1 = num2;
+num2 = num3;
+if((num1 % 2) == 0)
+total += num1;    
+}
+printf("%ld\n", total);
+return (0);
+}
