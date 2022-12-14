1bef969147746e998e9f9e3d86ac751e34d27973
babajideawoseemo <akinloluolaposi@gmail.com>
2022-12-13 19:02:14 -0800

    Even fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-unsigned long num1 = 0, num2 = 1, num3 = 0, total = 0;
-while (num3 < 400000)
+unsigned long num1 = 1, num2 = 2, num3, total = 0;
+while (num2 < 400000)
 {
-num3 = num2 + num1;
-num1 = num2;
-num2 = num3;
-if ((num1 % 2) == 0)
-total += num1;
+if(num2 % 2 == 0)
+total += num2;
+num3 = num2;
+num2 += num1;
+num1 = num3;
 }
 printf("%ld\n", total);
 return (0);
