30c7d2fe8cafd144c1a1e9b3cfaf94bc11476a1e
babajideawoseemo <akinloluolaposi@gmail.com>
2022-12-15 02:48:28 -0800

    Print uppercases

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _isupper - checks if a character is uppercase
+*@c: carrier integer variable
+*Return:  true, 0 false
+*/
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
