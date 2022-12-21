8077a539cb0bb869215855cf7accd73dc53a280f
babajideawoseemo <akinloluolaposi@gmail.com>
2022-12-21 09:40:37 -0800

    concatenate a string with further n value

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,10 +10,19 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-int length, i;
-length = 0;
-for (i = 0; src[i] != '\0' && i < n; ++i, ++length)
-dest[length] = src[i];
-dest[length] = '\0';
+int i, j;
+i = 0
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;       
+}
+dest[i] = '\0';
 return (dest);
 }
